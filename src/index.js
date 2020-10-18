87e830f4eb98923864b445a46abff986d6f5340c
Aleksey Kononenko <aleksey19988@gmail.com>
2020-10-18 13:53:36 +0300

    update project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/app.sass';
+
+const hamburger = document.querySelector('.header__hamburger');
+const textarea = document.getElementById('message');
+
+hamburger.onclick = () => {
+    const hamburgerTopBorder = document.querySelector('.header__hamburger__top_border');
+    const hamburgerBottomBorder = document.querySelector('.header__hamburger__bottom_border');
+    const headerMenu = document.querySelector('.header__menu');
+    
+    hamburgerTopBorder.classList.toggle('open__hamburger__top_border');
+    hamburgerBottomBorder.classList.toggle('open__hamburger__bottom_border');
+    headerMenu.classList.toggle('header__menu-open');
+};
+
+if (document.documentElement.clientWidth <= 768) {
+    textarea.removeAttribute(rows);
+    textarea.setAttribute(rows, 7);
+} else {
+
+};
